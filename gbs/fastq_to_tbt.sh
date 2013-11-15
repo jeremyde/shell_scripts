a9b83b3701a5990e2ab224f13aca9d32aeb1a3e0
jeremy <jeremy@localhost.localdomain>
2013-11-15 12:48:43 -0500

    shell script for fastq to tag count plugin

---
+++ gbs/fastq_to_tbt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#GBS analysis: Do FastqToTBT for each subdirectory in specified directory
+FILEDIR="$1"
+FILES="$FILEDIR*"
+OUTDIR="$2"
+KEYFILE="$3"
+enzyme="$4"
+TAGFILE="$5"
+for f in $FILES
+do
+    filename=$(basename "$f")
+    screen -d -m -S "$filename"  run_pipeline.pl -fork1 -FastqToTBTPlugin -i $f -o $OUTDIR -k $KEYFILE -y -e apeki -t $TAGFILE -endPlugin -runfork1
+done
