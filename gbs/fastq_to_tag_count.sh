b4e36b86d3852289297eef3f30bb2a70daa9e64c
Jeremy D. Edwards <jde22@cornell.edu>
2013-11-16 20:32:25 -0500

    echo commands that are run

--- gbs/fastq_to_tag_count.sh
+++ gbs/fastq_to_tag_count.sh
@@ -6,8 +6,10 @@ FILES="$FILEDIR*"
 OUTDIR="$2"
 KEYFILE="$3"
 enzyme="$4"
+echo "running commands:"
 for f in $FILES
 do
     filename=$(basename "$f")
-    screen -d -m -S "$filename" run_pipeline.pl "-fork1 -FastqToTagCountPlugin -i $f -o $OUTDIR -k $KEYFILE -e $enzyme -endPlugin -runfork1" &
+    echo "screen -d -m -S $filename run_pipeline.pl -fork1 -FastqToTagCountPlugin -i $f -o $OUTDIR -k $KEYFILE -e $enzyme -endPlugin -runfork1" 
+    screen -d -m -S "$filename" run_pipeline.pl "-fork1 -FastqToTagCountPlugin -i $f -o $OUTDIR -k $KEYFILE -e $enzyme -endPlugin -runfork1"
 done
--- gbs/fastq_to_tbt.sh
+++ gbs/fastq_to_tbt.sh
@@ -6,8 +6,10 @@ OUTDIR="$2"
 KEYFILE="$3"
 enzyme="$4"
 TAGFILE="$5"
+echo "Running commands:"
 for f in $FILES
 do
     filename=$(basename "$f")
+    echo "screen -d -m -S $filename  run_pipeline.pl -fork1 -FastqToTBTPlugin -i $f -o $OUTDIR -k $KEYFILE -y -e apeki -t $TAGFILE -endPlugin -runfork1"
     screen -d -m -S "$filename"  run_pipeline.pl -fork1 -FastqToTBTPlugin -i $f -o $OUTDIR -k $KEYFILE -y -e apeki -t $TAGFILE -endPlugin -runfork1
 done
